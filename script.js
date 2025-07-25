87112749623f53238cdde2ab137a7afda07d3044
R-Htu <htuaung89@gmail.com>
2025-07-25 13:40:17 +0630

    nav part 2 added

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-function toggleMode() {
 
-    const icon = document.getElementById('icon');
-    const thumb = document.getElementById('toggleThumb');
-    const root = document.documentElement;
-    const toggle = root.classList.toggle('dark');
- 
-    if(toggle) {
-        icon.classList.replace('fa-moon', 'fa-sun');
-        thumb.style.transform = "translateX(32px)";
-    } else {
-    
-        icon.classList.replace('fa-sun', 'fa-moon')
-        thumb.style.transform = "translateX(0)";
-    }
-}
